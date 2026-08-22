a8d9cbc8c5a179219bf11d463e63da87ccb95f20
square <kidusalamrewsqralex@gmail.com>
2026-08-22 15:17:16 +0300

    Update 6_About (ስለ).py

--- pages/6_About (ስለ).py
+++ pages/6_About (ስለ).py
@@ -68,4 +68,4 @@ from PIL import Image
 st.title("Buy Me a Coffee via telebirr ☕️")
 
 image = Image.open("telebirr_qr.png")
-st.image(image, caption="Scan to use Telebirr ", width = "stretch)
+st.image(image, caption="Scan to use Telebirr ", width = "stretch")
